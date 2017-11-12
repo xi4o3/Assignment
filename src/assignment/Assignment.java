242ed150be7c0fa130dcf9a885ed7598c76d22b2
MSI- <MSI-@MSI>
2017-11-12 17:43:52 +0800

    [finishes #152468988]

--- src/assignment/Assignment.java
+++ src/assignment/Assignment.java
@@ -99,11 +99,11 @@ public class Assignment {
  
  }
     
-    public static void clearScreen() {  
-    System.out.print("\033[H\033[2J");  
-    System.out.flush();  
-   }
-  
+//    public static void clearScreen() {  
+//    System.out.print("\033[H\033[2J");  
+//    System.out.flush();  
+//   }
+//  
 
 }
    
